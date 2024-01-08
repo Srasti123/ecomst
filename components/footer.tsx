b4f82478da22d9603223aca08d6f3a30c88ca512
Abhishek Singh <aahabhisheksingh@gmail.com>
2024-01-08 22:06:54 +0530

    store creation & gallery

---
+++ components/footer.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer className='bg-white border-t'>
+        <div className='mx-uto py-10'>
+            <p className='text-cente text-xs text-black'>
+                &copy; 2023 fakestoeName , Inc. All rights reserved
+            </p>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
